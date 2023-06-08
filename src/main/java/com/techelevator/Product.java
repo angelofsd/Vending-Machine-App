2a1dbb06ef8731ee4e48e860c187db7ec0bafd6a
Daniele Marsico <danymarsico@gmail.com>
2023-06-08 13:03:39 -0400

    Creates Product object

--- src/main/java/com/techelevator/Product.java
+++ src/main/java/com/techelevator/Product.java
@@ -1,5 +1,65 @@
 package com.techelevator;
 
-public abstract class Product {
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Product {
+    private String slotCode;
+    private String productName;
+    private BigDecimal productPrice;
+    private String productType;
+    private Integer productQuantity;
+
+    //Constructor
+    public Product(String slotCode, String productName, BigDecimal productPrice, String productType, Integer productQuantity) {
+        this.slotCode = slotCode;
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productType = productType;
+        this.productQuantity = productQuantity;
+    }
+
+
+    //Getters and Setters
+    public String getSlotCode() {
+        return slotCode;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public BigDecimal getProductPrice() {
+        return productPrice;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public Integer getProductQuantity() {
+        return productQuantity;
+    }
+
+    public void setSlotCode(String slotCode) {
+        this.slotCode = slotCode;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setProductPrice(BigDecimal productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public void setProductQuantity(Integer productQuantity) {
+        this.productQuantity = productQuantity;
+    }
 
 }
--- src/main/java/com/techelevator/VendingMachine.java
+++ src/main/java/com/techelevator/VendingMachine.java
@@ -1,78 +1,41 @@
 package com.techelevator;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.math.BigDecimal;
-import java.util.Scanner;
+import java.util.*;
 
 public class VendingMachine {
-    private String slotCode;
-    private String productName;
-    private BigDecimal productPrice;
-    private String productType;
     private int productQuantity=5;
-
-    //Getters and Setters
-    public String getSlotCode() {
-        return slotCode;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public BigDecimal getProductPrice() {
-        return productPrice;
-    }
-
-    public String getProductType() {
-        return productType;
-    }
-
-    public int getProductQuantity() {
-        return productQuantity;
-    }
-
-    public void setSlotCode(String slotCode) {
-        this.slotCode = slotCode;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public void setProductPrice(BigDecimal productPrice) {
-        this.productPrice = productPrice;
-    }
-
-    public void setProductType(String productType) {
-        this.productType = productType;
-    }
-
-    public void setProductQuantity(int productQuantity) {
-        this.productQuantity = productQuantity;
-    }
+    private List<Product> inventory = new ArrayList<>();
 
     private void loadVendingMachine(String filename) {
 
 
         try(Scanner inventoryScanner = new Scanner(new File(filename))) {
+
             while(inventoryScanner.hasNextLine()){
                 String line = inventoryScanner.nextLine();
                 String[] productInfo = line.split("\\|");
-                slotCode= productInfo[0];
-                productName= productInfo[1];
-                productPrice= new BigDecimal(productInfo[2]);
-                productType= productInfo[3];
+                String slotCode= productInfo[0];
+                String productName= productInfo[1];
+                BigDecimal productPrice= new BigDecimal(productInfo[2]);
+                String productType= productInfo[3];
+                Product product = new Product(slotCode, productName,productPrice,productType,productQuantity);
+                inventory.add(product);
+                //TODO put into a
 
             }
-        }catch(Exception ex){
-
+        }catch(IOException ex){
+            System.out.println("Sorry an error occurred while loading the inventory");
         }
+
 
 
     }
 
-    public VendingMachine {
+    public VendingMachine() {
         loadVendingMachine("vendingmachine.csv");
     }
 
