a3ac5467e1f23129b9c155e7cdd5ed62babb7068
Truong <rinzler1one@gmail.com>
2023-06-10 19:14:33 -0500

    optimized updateCoinsLeft() and updateCoins() methods so that it can calculate exact change even if the change is 35.31 for example by incrementing and displaying pennies in the updateCoins() method

--- src/main/java/com/techelevator/VendingMachine.java
+++ src/main/java/com/techelevator/VendingMachine.java
@@ -16,7 +16,6 @@ public class VendingMachine {
     private final int NICKEL = 5;
     private final int PENNY = 1;
 
-
     //Constructor
     public VendingMachine() {
         loadVendingMachine("vendingmachine.csv");
@@ -136,6 +135,8 @@ public class VendingMachine {
         int dimes = penniesAfterDollarAmount / DIME;
         penniesAfterDollarAmount %= DIME;
         int nickels = penniesAfterDollarAmount / NICKEL;
+        penniesAfterDollarAmount %= NICKEL;
+        int pennies = penniesAfterDollarAmount / PENNY;
 
         System.out.println("Thank you for choosing Vendo-Matic 800. Your CHANGE is: " + balance);
         System.out.println();
@@ -146,6 +147,8 @@ public class VendingMachine {
         System.out.println("Dimes: " + dimes);
         System.out.println("--------------------------");
         System.out.println("Nickels: " + nickels);
+        System.out.println("--------------------------");
+        System.out.println("Pennies: " + pennies);
         System.out.println("--------------------------");
 
     }
